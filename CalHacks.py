73c332135589a19881c3aa45d3785c05da1d05af
Abraham Chen <abraham.chen1@berkeley.edu>
2016-11-12 14:13:50 -0800

    Added all

---
+++ CalHacks.py
@@ -0,0 +1,149 @@
+import amadeus
+import json
+import urllib.request
+from lxml import html
+import requests
+import gmplot
+import googlemaps
+import re
+import urllib
+import geocoder
+
+period = '2014-01--2014-12'
+origin = 'TYO'
+destination = 'BKK'
+apikey = 'MOVlGTC47XulnCM32cPbOT5PSzmLfLhL'
+
+url = 'https://api.sandbox.amadeus.com/v1.2/' \
+      'travel-intelligence/' \
+      'flight-traffic?' \
+      'period={0}' \
+      '&origin={1}' \
+      '&destination={2}' \
+      '&apikey=MOVlGTC47XulnCM32cPbOT5PSzmLfLhL'.format(period,origin,destination)
+
+
+def get_json_data(url):
+    response = urllib.request.urlopen(url)
+    data = response.read().decode("utf-8")
+    #all_results = json.loads(data)["results"]
+    return json.loads(data)
+
+print(get_json_data(url)[0])
+
+def get_distance(start,end):
+    url = 'https://www.world-airport-codes.com/distance/?' \
+          'a1={0}' \
+          '&a2={1}'.format(start,end)
+    lines = urllib.request.urlopen(url).readlines()
+    x = "hello"
+    for line in lines:
+        if b"The distance has been calculated as being: " in line:
+            x = line
+    x = x.split()
+    x = x[9]
+    x = float(x[1:].decode("utf-8"))
+    print(x)
+
+global_set = {}
+def generate_centroids(js):
+    for flight_set in js:
+        if flight_set['destination'] in global_set:
+            global_set['destination'] += flight_set['travelers']
+        else:
+            global_set['destination'] = flight_set['travelers']
+
+
+#Takes in one city and generates all the surrounding airports within a 50 km radius
+def generate_region(centerCities, airportCodes, js):
+    cityAirports = {}
+
+    for city in centerCities:
+        for airportcode in js:
+            if nearCity(centerCity, airportcodes):
+
+                if cityAirports[city] in cityAirports:
+                    cityAirports.append(airportcodes)
+                else:
+                    cityAirports[city] = airportcodes
+
+
+def circle(self, lat, lng, radius, color=None, c=None, **kwargs):
+    color = color or c
+    kwargs.setdefault('face_alpha', 0.5)
+    kwargs.setdefault('face_color', "#000000")
+    kwargs.setdefault("color", color)
+    settings = self._process_kwargs(kwargs)
+    path = self.get_cycle(lat, lng, radius)
+    self.shapes.append((path, settings))
+
+### BREAK ###
+
+# Finds the city the airport is in
+def findcity(airportcode, url = "http://airportcodes.org/"):
+    response = urllib.request.urlopen(url)
+    data = response.read().decode("utf-8")
+    m = re.search('(.*),\s([A-Z]{2}).*\s\((' + airportcode + ')\)', data)
+    return m.group(1)
+
+# Finds the state that the airport is in
+def findstate(airportcode, url = "http://airportcodes.org/"):
+    response = urllib.request.urlopen(url)
+    data = response.read().decode("utf-8")
+    m = re.search('(.*),\s([A-Z]{2}).*\s\((' + airportcode + ')\)', data)
+    return m.group(2)
+
+# Finds the state that the city is in
+def statefromcity(city, url = "http://airportcodes.org/"):
+    response = urllib.request.urlopen(url)
+    data = response.read().decode("utf-8")
+    m = re.search('('+ city +'),\s([A-Z]{2})', data)
+    return m.group(2)
+
+def get_airportCodes(url = "http://airportcodes.org/"):
+    airCodes = set()
+
+    response = urllib.request.urlopen(url)
+    data = response.read().decode("utf-8")
+    pattern = re.compile('(.*),\s([A-Z]{2}).*\s\((.*)\)')
+
+    for m in re.finditer(pattern, data):
+        airCodes.add(m.group(3))
+
+    print(airCodes)
+
+# Returns the lat and long of an aiport
+def googleAirport(airportcode):
+    g = geocoder.google(findcity(airportcode)+ ", "+ findstate(airportcode))
+    TupesCoordinate = g.latlong
+    return TupesCoordinate
+
+# Returns the lat and long of a city
+def googleCity(city):
+    g = geocoder.google(city + ", "+ statefromcity(city))
+    TupesCoordinate = g.latlong
+    return TupesCoordinate
+
+### BREAK ###
+
+def inRange(center_x, center_y, lat, lon, radius):
+    return (lat - center_x)**2 + (lon - center_y)**2 < radius**2
+
+# 0.45 coorespondes to a range of 50 km
+def nearCity(centerCity, airport, radius = 0.45):
+    airportlat = googleAirport(airport)[0]
+    airportlong = googleAirport(airport)[1]
+    centerCitylat = googleCity(centerCity)[0]
+    centerCitylong = googleCity(centerCity)[1]
+    return inRange(centerCitylat, centerCitylong, airportlat, airportlong)
+
+
+# DUMP
+
+
+#Test Cases
+
+
+
+
+
